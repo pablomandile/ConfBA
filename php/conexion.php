74a3fe1552a54992c0aa8f48c5c119d9f7f9f04d
Pablo Mandile <pablo.mandile@gmail.com>
2021-12-08 21:28:07 -0300

    listado de usuarios de la tabla registro

--- php/conexion.php
+++ php/conexion.php
@@ -6,16 +6,24 @@ $claveBD = "";
 
 $conexionBD = mysqli_connect($lugarBD, $usuarioBD, $claveBD, $nombreBD);
 if (mysqli_connect_errno()){
-    echo "Se produjo un error en la conexión a la base de datos: ".mysqli_connect_errno();
+    echo "Se produjo un error en la conexión a la base de datos: ".mysqli_connect_errno()."<br>";
 }else{
-    echo "Se conectó a la base de datos correctamente";
+    echo "Se conectó a la base de datos correctamente <br>";
 }
 
 $consultaTodos = "SELECT * FROM registro";
-
-
-$consultas = mysqli_query($conexion,$consultaTodos);
-
-echo "<pre>";
+$consultas = mysqli_query($conexionBD,$consultaTodos);
+/*echo "<pre>";
 var_dump($consultas);
-echo "</pre>";
+echo "</pre>";*/
+while($listadoUsuarios = mysqli_fetch_array($consultas)){
+    echo $listadoUsuarios['id_usuario']." ";
+    echo $listadoUsuarios['nombre']." ";
+    echo $listadoUsuarios['apellido']." ";
+    echo $listadoUsuarios['dni']." ";
+    echo $listadoUsuarios['telefono']." ";
+    echo $listadoUsuarios['correo']." ";
+    echo "<br>";
+}
+
+?>
